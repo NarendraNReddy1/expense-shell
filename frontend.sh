83e192942a931f00e4608e52f1396c0608c656b4
NarendraNReddy1 <narendra.h1b@gmail.com>
2024-08-14 05:55:04 -0700

    All

---
+++ frontend.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+USER_ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+
+
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 
+    else 
+        echo -e "$2...$G SUCCESS $N"    
+
+    fi
+}
+
+if [ $USER_ID -ne 0 ]
+then
+    echo "Please be a super user"
+    exit 1
+else 
+    echo "You are super user" 
+fi 
+
+
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing nginx"
+
+systemctl enable nginx &>>$LOG_FILE
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx &>>$LOG_FILE
+VALIDATE $? "Start nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+VALIDATE $? "Remove original HTML"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading frontend code"
+
+cd /usr/share/nginx/html &>>$LOG_FILE
+VALIDATE $? "Moving to html directory"
+
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "Unzipping"
+
+cp -rf /home/ec2-user/expense-shell /etc/nginx/default.d/expense.conf &>>$LOG_FILE
+VALIDATE $? "Copying"
+
+systemctl restart nginx &>>$LOG_FILE
+VALIDATE $? "Nginx restart"
+
+
+
+
+
+
+
